4449b34a3b1304c05d3794ddb8c84a6bdf3fc4c9
ivandhar <ivandarma98@gmail.com>
2021-06-30 13:31:14 +0700

    coni

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://youtubetest:k9PAKsm9iJckQtBk@cluster0.huk64.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
